d384db9c1ab485492bc78a62b82c964a87026b83
markomih1 <mmihovilo@pmfst.hr>
2023-02-08 10:36:22 +0100

    nestoo

--- vj14/z1/Analyzer.cpp
+++ vj14/z1/Analyzer.cpp
@@ -17,9 +17,9 @@ void Analyzer::Funkcija()
 
  gStyle->SetOptStat(0);
  file = new TFile("/home/public/data/Lifetime/Lifetime.root");
-  tree = (TTree*)file->Get("Tree");
+  tree = (TTree*)file->Get("tree");
   tree->SetBranchAddress("var",&var);
-  HistoLikelihood = new TH1F("Likelihood", "Likelihood", 1, 0.0, 50.);
+  HistoLikelihood = new TH1F("Likelihood", "Likelihood", 50, 0.0, 10.);
 
  funkc= new TF1("funkcija","[0]*(exp(-x/4.)+exp(-1*TMath::Power((x-3.),2)/2*0.09)",0.1,10.);
  funkc->SetParameter(0,100.);
@@ -35,8 +35,7 @@ nentries=tree->GetEntries();
  //HistoLikelihood->Fit(funkc,"L");
 
   canvas1=new TCanvas("canvas1","canvas1",600,400);
-  HistoLikelihood->GetXaxis()->SetTitle("t/s");
-  HistoLikelihood->GetYaxis()->SetTitle("N");
+
   HistoLikelihood->Draw();
   canvas1->SaveAs("Lifelihood.pdf");
 
