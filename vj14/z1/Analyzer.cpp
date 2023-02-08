91af1a53558def6614c9f16aa657618c68e0729a
markomih1 <mmihovilo@pmfst.hr>
2023-02-08 11:18:35 +0100

    qwd

--- vj14/z1/Analyzer.cpp
+++ vj14/z1/Analyzer.cpp
@@ -19,14 +19,14 @@ void Analyzer::Funkcija()
  file = new TFile("/home/public/data/Toy/ToyTree.root");
   tree = (TTree*)file->Get("tree");
   tree->SetBranchAddress("var",&var);
-  HistoLikelihood = new TH1F("Likelihood", "Likelihood", 50, 0.0, 10.);
+  HistoLikelihood = new TH1F("Likelihood", "Likelihood", 100, 0.0, 10.);
 
-// funkc= new TF1("funkcija","[0]* (TMath::Exp(-x/[1])+TMath::Exp(-(x-[2])*(x-[2])/(2.*[3]*[3]))",0.1,10.);
- //funkc->SetParameter(0,100.);
-//funkc->SetParameter(1,4.);
-//funkc->SetParameter(2,3.);
-//funkc->SetParameter(3,0.3);
- 
+funkc= new TF1("funkcija","[0]* (TMath::Exp(-x/[1])+TMath::Exp(-(x-[2])*(x-[2])/(2.*[3]*[3])))",0.,10.);
+ funkc->SetParameter(0,224.);
+funkc->SetParameter(1,4.);
+funkc->SetParameter(2,3.);
+funkc->SetParameter(3,0.3);
+//funkc->Draw(); 
 Long64_t nentries=tree->GetEntries();
   for(i=0;i<nentries;i++)
   {
@@ -35,7 +35,7 @@ Long64_t nentries=tree->GetEntries();
     
   }
 
- //HistoLikelihood->Fit(funkc,"L");
+HistoLikelihood->Fit(funkc,"L","",0,10);
 
   canvas1=new TCanvas("canvas1","canvas1",600,600);
 
