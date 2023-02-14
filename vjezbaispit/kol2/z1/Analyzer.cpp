7f3845cea5d64b9cad4e289f47b5ab6bc7171fe3
markomih1 <mmihovilo@pmfst.hr>
2023-02-14 19:47:21 +0100

    dqwd

--- vjezbaispit/kol2/z1/Analyzer.cpp
+++ vjezbaispit/kol2/z1/Analyzer.cpp
@@ -17,12 +17,12 @@ gStyle->SetOptStat(0);
   tree = (TTree*)file->Get("tree");
   tree->SetBranchAddress("var",&var);
   histo = new TH1F("Toy", "Toy", 50, 0.1, 10.0);
-  f= new TF1("funkcija","[0] * (TMath::Exp(-(x/[1]))*TMath::Exp(-(x*[3])*(x-[3])/2/[4]/[4]))",0.1,10.);
+  f= new TF1("fit funkcija", "[0] * (TMath::Exp(-x / [1]) + TMath::Exp(-(x - [2]) * (x - [2]) / 2. / [3] / [3]))", 0, 10);
 
-f->SetParameter(0,440.);
+f->SetParameter(0,224.);
 f->SetParameter(1,4.);
-f->SetParameter(3,3.);
-f->SetParameter(4,0.3);
+f->SetParameter(2,3.);
+f->SetParameter(3,0.3);
 
 Long64_t nentries=tree->GetEntries();
   for(i=0;i<nentries;i++)
